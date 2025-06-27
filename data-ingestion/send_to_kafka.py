314f47da8063b7e3db69f710695ca54be3911271
carlostrosales <rosales.carlost@Gmail.com>
2025-06-27 16:02:32 -0500

    fix: update Kafka broker address and enhance completion message with event count and duration

--- data-ingestion/send_to_kafka.py
+++ data-ingestion/send_to_kafka.py
@@ -6,7 +6,7 @@ from kafka import KafkaProducer
 
 # --- config ---
 KAFKA_TOPIC = 'gh-events'
-KAFKA_BROKER = 'localhost:9092'
+KAFKA_BROKER = 'kafka:9092'
 ARCHIVE_URL = "https://data.gharchive.org/2024-01-01-0.json.gz"
 
 def main():
@@ -14,6 +14,7 @@ def main():
     producer = KafkaProducer(bootstrap_servers=KAFKA_BROKER, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
     # --- Download, decompress, read/send event to kafka topic
+    start_time = time.time()
     print("Downloading archive...")
     response = requests.get(ARCHIVE_URL, stream=True)
 
@@ -22,15 +23,18 @@ def main():
     # use this pattern when downloading from a URL, streaming the content directly (i.e. avoiding saving to disk), or wanting to decompress on the fly
     # as 's' gives access to the uncompressed stream via a variable
     with gzip.GzipFile(fileobj=response.raw) as s:
+        count = 0
         for i, line in enumerate(s):
             try:
                 event = json.loads(line.decode("utf-8"))
                 producer.send(KAFKA_TOPIC, event)
+                count += 1
             except Exception as e:
                 print("Error:", e)
 
     producer.flush()
-    print("Done sending events to kafka topic.")
+    duration = time.time() - start_time
+    print(f"Done sending events to Kafka. Sent {count} events in {duration:.2f} seconds.")
 
 if __name__ == "__main__":
     main()
